baf75b209a35ed43c26826d3ce3bdd86b43b72fb
Larabia <lorena.arabia@gmail.com>
2022-08-05 17:57:12 -0300

    Declare three arrays for the haiku components

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log('hello haiku!');
+/*------------------- Declarations -------------------*/
+//haiku components
+const firstLine = ['test1','test2','test3'];
+const secondLine = ['test1','test2','test3','test4'];
+const thirdLine = ['test1','test2','test3'];
